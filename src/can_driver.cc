0b2c849345201e7c05f47d94ed0b68798a20f5a2
Alexi Demers <alexidemers@gmail.com>
2015-11-25 22:55:31 -0500

    CanDriver class created

---
+++ src/can_driver.cc
@@ -0,0 +1,59 @@
+/**
+ * \file	sonar_driver.cc
+ * \author	Alexi Demers <alexidemers@gmail.com>
+ * \date	25/11/2015
+ *
+ * \copyright Copyright (c) 2015 Copyright
+ *
+ * \section LICENSE http://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Changes by: S.O.N.I.A.
+ * \copyright Copyright (c) 2015 S.O.N.I.A. All rights reserved.
+ *
+ * \section LICENSE
+ *
+ * This file is part of S.O.N.I.A. software.
+ *
+ * S.O.N.I.A. software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * S.O.N.I.A. software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with S.O.N.I.A. software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "can_driver.h"
+
+namespace provider_can {
+
+//==============================================================================
+// C / D T O R   S E C T I O N
+
+//------------------------------------------------------------------------------
+//
+    CanDriver::CanDriver(void)
+    {
+
+    }
+
+//------------------------------------------------------------------------------
+//
+    CanDriver::~CanDriver() { }
+
+//==============================================================================
+// M E T H O D S   S E C T I O N
+
+//------------------------------------------------------------------------------
+//
+
+
+//------------------------------------------------------------------------------
+//
+
+} // namespace provider_sonar
