59bd8d49b95cb87bf938e553a2a30a2090aa368c
S.O.N.I.A. Guest <sonia@ens.etsmtl.ca>
2016-06-26 17:14:19 -0400

    Adding a loop rate to the main node to reduce a bit the process time. Still at 500 hz though...

--- src/provider_can/can_node.cc
+++ src/provider_can/can_node.cc
@@ -38,76 +38,73 @@ CanNode::CanNode(const ros::NodeHandlePtr &nh) ATLAS_NOEXCEPT
       devices_conf_(nh_),
       can_ptr_(nullptr),
       can_devices_vector_({}) {
-
   can_ptr_ = std::make_shared<provider_can::CanDispatcher>(
       conf_.device_id, conf_.unique_id, conf_.channel, conf_.baudrate,
       conf_.usb_device, nh_);
 
-
-
   // initialize all new devices here
 
-  if(devices_conf_.bottom_light_en_)
+  if (devices_conf_.bottom_light_en_)
     can_devices_vector_.push_back(
         std::make_shared<provider_can::BottomLight>(can_ptr_, nh_));
 
-  if(devices_conf_.power_supply_en_)
-    can_devices_vector_.push_back(
-        std::make_shared<provider_can::PowerSupply>(can_ptr_, nh_, devices_conf_.psu_params_));
+  if (devices_conf_.power_supply_en_)
+    can_devices_vector_.push_back(std::make_shared<provider_can::PowerSupply>(
+        can_ptr_, nh_, devices_conf_.psu_params_));
 
-  if(devices_conf_.thruster_port_en_)
+  if (devices_conf_.thruster_port_en_)
     can_devices_vector_.push_back(std::make_shared<provider_can::Thruster>(
         can_ptr_, nh_, "port", port_motor));
 
-  if(devices_conf_.thruster_front_depth_en_)
+  if (devices_conf_.thruster_front_depth_en_)
     can_devices_vector_.push_back(std::make_shared<provider_can::Thruster>(
         can_ptr_, nh_, "front_depth", front_depth_motor));
 
-  if(devices_conf_.thruster_back_depth_en_)
+  if (devices_conf_.thruster_back_depth_en_)
     can_devices_vector_.push_back(std::make_shared<provider_can::Thruster>(
         can_ptr_, nh_, "back_depth", back_depth_motor));
 
-  if(devices_conf_.thruster_front_head_en_)
+  if (devices_conf_.thruster_front_head_en_)
     can_devices_vector_.push_back(std::make_shared<provider_can::Thruster>(
         can_ptr_, nh_, "front_heading", front_heading_motor));
 
-  if(devices_conf_.thruster_back_head_en_)
+  if (devices_conf_.thruster_back_head_en_)
     can_devices_vector_.push_back(std::make_shared<provider_can::Thruster>(
         can_ptr_, nh_, "back_heading", back_heading_motor));
 
-  if(devices_conf_.thruster_starboard_en_)
+  if (devices_conf_.thruster_starboard_en_)
     can_devices_vector_.push_back(std::make_shared<provider_can::Thruster>(
         can_ptr_, nh_, "starboard", starboard_motor));
 
-  if(devices_conf_.barometer_en_)
+  if (devices_conf_.barometer_en_)
     can_devices_vector_.push_back(
         std::make_shared<provider_can::Barometer>(can_ptr_, nh_));
 
-  if(devices_conf_.grabber_en_)
+  if (devices_conf_.grabber_en_)
     can_devices_vector_.push_back(
         std::make_shared<provider_can::Grabber>(can_ptr_, nh_));
 
-  if(devices_conf_.diver_interface_en_)
+  if (devices_conf_.diver_interface_en_)
     can_devices_vector_.push_back(
         std::make_shared<provider_can::DiverInterface>(can_ptr_, nh_));
 
-  if(devices_conf_.mission_switch_en_)
+  if (devices_conf_.mission_switch_en_)
     can_devices_vector_.push_back(
         std::make_shared<provider_can::MissionSwitch>(can_ptr_, nh_));
 
-  if(devices_conf_.led_indicator_en_)
+  if (devices_conf_.led_indicator_en_)
     can_devices_vector_.push_back(
         std::make_shared<provider_can::LedIndicator>(can_ptr_, nh_));
 
-  if(devices_conf_.hydrophones_en_)
-    can_devices_vector_.push_back(
-        std::make_shared<provider_can::Hydrophones>(can_ptr_, nh_, devices_conf_.hydros_params_));
+  if (devices_conf_.hydrophones_en_)
+    can_devices_vector_.push_back(std::make_shared<provider_can::Hydrophones>(
+        can_ptr_, nh_, devices_conf_.hydros_params_));
 
-  if(devices_conf_.torpedo_launcher_en_)
+  if (devices_conf_.torpedo_launcher_en_)
     can_devices_vector_.push_back(
         std::make_shared<provider_can::TorpedoLaunchers>(can_ptr_, nh_));
 
-  if(devices_conf_.droppers_en_)
+  if (devices_conf_.droppers_en_)
     can_devices_vector_.push_back(
         std::make_shared<provider_can::Droppers>(can_ptr_, nh_));
 
@@ -124,11 +121,13 @@ CanNode::~CanNode() ATLAS_NOEXCEPT {}
 //------------------------------------------------------------------------------
 //
 void CanNode::Run() ATLAS_NOEXCEPT {
+  ros::Rate loop_rate(500);
   while (!ros::isShuttingDown()) {
     ros::spinOnce();
     for (auto &device : can_devices_vector_) {
       device->Process();
     }
+    loop_rate.sleep();
   }
 }
 
